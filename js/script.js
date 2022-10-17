bade38fb491f7646b4c47750304a0654a70a236c
pannellalino <lista>
2022-10-17 10:05:43 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -17,3 +17,19 @@ if(mailPresente === true){
 }else{
   output.innerHtml = 'no';
 }
+
+
+//es 2
+
+const numeroA = Math.ceil(Math.random() * 6);
+const numeroB = Math.ceil(Math.random() * 6);
+
+let msg ='';
+
+if(numeroA > numeroB){
+  msg = 'A vince';
+}else if(numeroA < numeroB){
+  msg = 'B vince'
+}else{
+  msg = 'pari';
+};
